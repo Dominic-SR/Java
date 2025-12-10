233a478373da8348b161b3eb86fd10d538e3404f
Dominic-SR <dominicseduraja3@gmail.com>
2025-12-11 02:51:50 +0530

    basic commit

---
+++ basics/userinput/userinput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class userinput{
+ public static void main(String args[]){
+   Scanner dom = new Scanner(System.in);
+   int a = dom.nextInt();
+   String b = dom.nextLine();
+
+   System.out.print(a+b);
+
+ }
+}
---
+++ basics/variables/variables.java
@@ -0,0 +1,7 @@
+class variables{
+ public static void main(String args[]){
+    int num = 3;
+    String word = "hello";
+  System.out.print(num+" "+word);
+  }
+}
